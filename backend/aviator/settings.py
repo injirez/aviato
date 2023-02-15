40bca10e91a9cf8d9a9ea2f051037d31b3ab7bde
injirez <injirka@outlook.com>
2023-02-15 14:30:00 +0300

    Add registration

--- backend/aviator/settings.py
+++ backend/aviator/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-^%q9&-u2io21s@g7$#-l%upn#9q(3+7h&br3fnjldl62ngg6b1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition

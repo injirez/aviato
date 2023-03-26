3a84ae6faacdb3d9b13b6c45bcafcdefaaada76d
Rodion Ibragimov <rodion.ibragimov@red-soft.ru>
2023-03-26 17:53:34 +0300

    Rework profile apis

--- backend/client/views.py
+++ backend/client/views.py
@@ -1,8 +1,6 @@
-from django.shortcuts import render
 from .models import Profile
 from django.shortcuts import get_object_or_404
-from .serializers import ProfileSerializer, ProfileInfoSerializer
-from rest_framework.viewsets import ViewSet
+from .serializers import ProfileSerializer, UserSerializer
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from django.contrib.auth.models import User
@@ -11,21 +9,33 @@ from django.contrib.auth.models import User
 class ProfileAPIView(APIView):
 
     def get(self, request):
-        user_instance = User.objects.get(pk=request.user.id)
+        """
+        Get user profile info
+        Return profile object
+        or user object if profile does not exist
+        """
+        user_instance = get_object_or_404(User, pk=request.user.id)
         try:
             queryset = Profile.objects.get(user=user_instance)
-            serializer = ProfileInfoSerializer(queryset, many=False)
+            serializer = ProfileSerializer(queryset, many=False)
 
             return Response({'response': serializer.data})
         except Profile.DoesNotExist:
-            return Response({'response': {}})
-
+            serializer = UserSerializer(user_instance, many=False)
+            return Response({'response': {'user': serializer.data}})
 
     def post(self, request):
-        user_instance = User.objects.get(pk=request.user.id)
+        """
+        Edit user profile
+        user.username, user.first_name,
+        user.last_name, user.email,
+        phone: +7, photo: filefield
+        Return profile object
+        """
+        user_instance = get_object_or_404(User, pk=request.user.id)
         try:
             profile = Profile.objects.get(user=user_instance)
-            serializer = ProfileInfoSerializer(instance=profile, data=request.data)
+            serializer = ProfileSerializer(instance=profile, data=request.data)
             serializer.is_valid(raise_exception=True)
         except Profile.DoesNotExist:
             serializer = ProfileSerializer(data=request.data, context={'user': user_instance})
